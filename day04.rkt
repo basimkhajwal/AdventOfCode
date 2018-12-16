758548cbe5508fa3e6bf0ec4f4cb0b15d457e05a
Basim Khajwal <basimkhajwal@Basims-MBP.home>
2018-12-16 17:37:12 +0000

    Solved day4

--- day04.rkt
+++ day04.rkt
@@ -2,49 +2,29 @@
 
 (require "common.rkt")
 
-;(define (make-my-date year month day hour minute)
-;  (make-date 0 minute hour day month year 6 348 #f 0))
-;
-;(define (get-date-time ln)
-;  (define m (regexp-match #rx"^\\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\\]" ln))
-;  (apply make-my-date (map string->number (rest m))))
-
 (define (max-by xs f [lt <])
-  (define-values (a xs*) (split-at xs 1))
+  (define-values (a xs*) (values (first xs) (rest xs)))
   (define (step x y acc) (if (lt (cdr acc) y) (cons x y) acc))
-  (car (foldl step (cons (car a) (f (car a))) xs* (map f xs*))))
+  (car (foldl step (cons a (f a)) xs* (map f xs*))))
 
-(define (part-one input)
+(define (max-hash-key hash #:by [f identity] [lt <])
+  (car (max-by (hash->list hash) cdr)))
 
-  (define (append-time-asleep change-times sleep-hash)
-    (define next-sleep (if (empty? change-times) 60 (first change-times)))
-    (define next-awake (if (< (length change-times) 2) 60 (cadr change-times)))
-    (for ([t (in-range next-sleep next-awake)])
-      (hash-update! sleep-hash t add1 0))
-    (if (< next-awake 60)
-        (append-time-asleep (drop change-times 2) sleep-hash)
-        void))
+(define (max-hash-value hash) (apply max (cons 0 (hash-values hash))))
 
-  (define (time-asleep guard-times)
-    (define sleep-hash (make-hash))
-    (for ([change-times (in-list guard-times)]) (append-time-asleep change-times sleep-hash))
-    (sleep-hash))
-
+(define (part-one guard-sleep-times)
   (define (total-time-asleep sleep-hash)
     (for/sum ([cnt (in-hash-values sleep-hash)]) cnt))
-
-  (define (collect guard guards)
-    (hash-update guards (first guard)
-                 (lambda (xs) (cons (rest guard) xs))
-                 empty))
-
-  (define collected-guards (foldl collect (make-immutable-hash) input))
-
-  (define guard-sleep-times void)
-  ; TODO finish this
-  "")
-
   
+  (define max-guard (max-by guard-sleep-times (compose total-time-asleep cdr)))
+  
+  (* (car max-guard) (max-hash-key (cdr max-guard))))
+
+(define (part-two guard-sleep-times)
+  (define max-guard
+    (max-by guard-sleep-times (compose max-hash-value cdr)))
+  (define max-time (max-hash-key (cdr max-guard)))
+  (* (car max-guard) max-time))
 
 (define (parse-file file)
   (define lines (sequence->list (in-lines file)))   
@@ -66,6 +46,34 @@
            (cons (get-guard-id (first xs)) (map get-minute guard-items))
            (split-guards xs*)))))
   
-  (split-guards (sort lines #:key extract-date string<?)))
+  (define guard-data (split-guards (sort lines #:key extract-date string<?)))
 
-((multipart-question "input/04.txt" parse-file #:name "Day04") part-one)
+  (define (collate-guards input)
+    (define (collect guard guards)
+      (hash-update guards (first guard)
+                   (lambda (xs) (cons (rest guard) xs))
+                   empty))
+    (foldl collect (make-immutable-hash) input))
+
+  (define (append-time-asleep change-times sleep-hash)
+    (define next-sleep (if (empty? change-times) 60 (first change-times)))
+    (define next-awake (if (< (length change-times) 2) 60 (cadr change-times)))
+    (define sleep-hash*
+      (for/fold ([current-hash sleep-hash])
+                ([t (in-range next-sleep next-awake)])
+        (hash-update current-hash t add1 0)))
+    (if (< next-awake 60)
+        (append-time-asleep (drop change-times 2) sleep-hash*)
+        sleep-hash))
+
+  (define (time-asleep guard-times)
+    (for/fold ([sleep-hash (make-immutable-hash)])
+              ([change-times (in-list guard-times)])
+      (append-time-asleep change-times sleep-hash)))
+
+  (define guard-sleep-times
+    (hash-map (collate-guards guard-data)
+              (lambda (g xs) (cons g (time-asleep xs)))))
+  guard-sleep-times)
+
+((multipart-question "input/04.txt" parse-file #:name "Day04") part-one part-two)
